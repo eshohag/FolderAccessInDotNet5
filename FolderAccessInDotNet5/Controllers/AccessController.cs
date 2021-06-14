939f1a87148d414222dd4c4a750c53f1cc905df6
Md Shohag Mia <shohag.mia@datasoft-bd.com>
2021-06-14 16:59:10 +0600

    Add project files.

---
+++ FolderAccessInDotNet5/Controllers/AccessController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace FolderAccessInDotNet5.Controllers
+{
+    public class AccessController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+
+        public AccessController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public IActionResult Index()
+        {
+            //string modelsFolderAccess =Path.Combine(Directory.GetCurrentDirectory(), "Models");    //Old Way
+            string contentRootPath = Path.Combine(_environment.ContentRootPath, "Models");
+            string webRootPath = Path.Combine(_environment.WebRootPath, "js");
+
+            string[] filePaths = Directory.GetFiles(contentRootPath);
+
+            return View();
+        }
+    }
+}
